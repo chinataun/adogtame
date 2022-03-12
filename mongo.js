c3ff2510126e4a75a2d88821d9737bf198e504e7
jvelez <jvelez@idealista.com>
2022-03-12 23:24:40 +0100

    a

--- mongo.js
+++ mongo.js
@@ -2,7 +2,7 @@ require('dotenv').config({ path: './.env' });
 const mongoose = require ('mongoose')
 
 // const connectionString = `mongodb+srv://adogtame:adogtameDB@clusteradogtame.zonvg.mongodb.net/adogtame?retryWrites=true&w=majority`
-const connectionString = process.env.MONGO_DB_URI
+const connectionString = process.env.MONGO_DB_URI.toString()
 //conexion a mongodb
 mongoose.connect(connectionString, {
 	useUnifiedTopology: true,
