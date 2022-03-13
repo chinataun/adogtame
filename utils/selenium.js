79744c679f18e879e72a1bc328febc5ccc91fe51
jesusvelez <jesusvelez@ucm.es>
2022-03-13 22:32:45 +0100

    funcionando

--- utils/selenium.js
+++ utils/selenium.js
@@ -1,13 +1,11 @@
-const {Builder, By, Key} = require('selenium-webdriver')
+const {Builder, By, Key, Actions} = require('selenium-webdriver')
 
 async function example() {
     let driver = await new Builder().forBrowser("chrome").build();
     await driver.get("https://adogtame-gps.herokuapp.com")
 
     await driver.findElement(By.name("nombre")).sendKeys('Adrian', Key.TAB)
-    await driver.findElement(By.id("nombreM")).click()
-console.log(driver.findElement(By.id("nombreM")))
-     await driver.findElement(By.name("rango")).sendKeys("54", Key.TAB)
+    await driver.findElement(By.css("input[id='nombreM']")).click()
     await driver.sleep(3000)
 
     await driver.findElement(By.id("crearSubmit")).click()
