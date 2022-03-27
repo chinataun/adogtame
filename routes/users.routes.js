f97afb329e07887f762c4faf148db4fbdc4ae893
Chang <chcong@ucm.es>
2022-03-28 00:12:22 +0200

    ADOG-146: añadida las rutas

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,7 +1,10 @@
 const router = require('express').Router()
 
 const {renderRegistro, registro} =require('../controllers/users.controller')
+const {renderRegistro, registro,renderRegistroProtectora, registroProtectora} = require('../controllers/users.controller')
 
+router.get('/registro/protectora', renderRegistroProtectora)
+router.post('/registro/protectora', registroProtectora)
 // Routes
 router.get("/registro", renderRegistro);
 router.post("/registro", registro);
