263f3fd4ecb4fd2246bfe5c56ad03918bd8b08b3
Jesus <jesusvelez@ucm.es>
2022-03-29 01:55:17 +0200

    retoqes mongo

--- mongo.js
+++ mongo.js
@@ -5,16 +5,30 @@ const connectionString = NODE_ENV === 'development'
 	? 'mongodb+srv://adogtame:adogtameDB@clusteradogtame.zonvg.mongodb.net/adogtame-staging?retryWrites=true&w=majority'
 	: 'mongodb+srv://adogtame:adogtameDB@clusteradogtame.zonvg.mongodb.net/adogtame?retryWrites=true&w=majority'
   
-const conection = mongoose.connect('mongodb+srv://adogtame:adogtameDB@clusteradogtame.zonvg.mongodb.net/adogtame-staging?retryWrites=true&w=majority', {
-	useUnifiedTopology: true,
-	useNewUrlParser: true,
-})
-.then(() => {
-	console.log('conexion establecida a la base de datos')
-})
-.catch(err => {
-	console.log(err)
-})
+let conection;
+if (NODE_ENV === 'development') {
+	conection = mongoose.connect('mongodb+srv://adogtame:adogtameDB@clusteradogtame.zonvg.mongodb.net/adogtame-staging?retryWrites=true&w=majority', {
+		useUnifiedTopology: true,
+		useNewUrlParser: true,
+	}).then(() => {
+		console.log('conexion establecida a la base de datos')
+	})
+	.catch(err => {
+		console.log(err)
+	})
+} else {
+	conection = mongoose.connect('mongodb+srv://adogtame:adogtameDB@clusteradogtame.zonvg.mongodb.net/adogtame?retryWrites=true&w=majority', {
+		useUnifiedTopology: true,
+		useNewUrlParser: true,
+	}).then(() => {
+		console.log('conexion establecida a la base de datos')
+	})
+	.catch(err => {
+		console.log(err)
+	})
+}
+
+
 
 process.on('uncaughtException', error => {
   console.error(error)
