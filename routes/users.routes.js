8f16efba47fe58f20a5d9f78f759d9276524beeb
Yo <adrianig@ucm.es>
2022-04-18 14:21:53 +0200

    ADOG-169: routing

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,11 +1,13 @@
 const router = require('express').Router()
 const {renderRegistro, registro,renderRegistroProtectora, registroProtectora, renderLogin, login} = require('../controllers/users.controller')
-
+const {renderProtectoras} = require('../controllers/user.protectora.controller')
 router.get("/registro", renderRegistro);
 router.post("/registro", registro);
 
+//PROTECTORAS
 router.get('/registro/protectora', renderRegistroProtectora)
 router.post('/registro/protectora', registroProtectora)
+router.get("/protectoras", renderProtectoras);
 
 router.get("/login", renderLogin);
 router.post("/login", login);
