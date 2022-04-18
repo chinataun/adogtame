420bab6d94c2497e9d6b0b09d829cdef9de5c873
Yo <adrianig@ucm.es>
2022-04-18 17:44:38 +0200

    ADOG-214: controlador adoptante y refactor de controladores.

---
+++ controllers/user.adoptante.controller.js
@@ -0,0 +1,42 @@
+const Adoptante = require('../models/Adoptante')
+const User = require('../models/User')
+const validator = require('../utils/service.validations')
+
+const renderRegistroAdoptante =  (request, response) => {
+  response.redirect('/users/registro')
+}
+
+const registroAdoptante = async (request, response) => {
+  const { email, dni, telefono, descripcion, nombre, password, role } = request.body;
+  const {file} = request
+  // if (!validatorAdoptante.validateNombreAdoptante(nombre)) errors.push('El nombre debe ser superior a 4 caracteres'); 
+  // validatorAdoptante.validateTelefonoAdoptante(telefono)
+ 
+  /*const validation = validateAdoptante(request)
+  if (Object.keys(validation).length !== 0) {
+    return response.render('users/signup_adoptante', {errors: validation, email, dni, telefono, descripcion, nombre, password})
+  }*/
+
+  const newAdoptante = new Adoptante({ 
+    nombre: nombre, 
+    dni: dni, 
+    telefono: telefono, 
+    descripcion: (descripcion == '') ? undefined : descripcion,
+    image: (file == undefined) ? file : file.filename,
+  });
+  const adoptantesaved = await newAdoptante.save();
+
+  const newUser = new User({
+    email: email,
+    password: password,
+    role: role,
+    user: adoptantesaved._id,
+  })
+  newUser.password = await newUser.encryptPassword(password);
+  await newUser.save();
+
+  request.flash("success_msg", `Usuario ${role} con email: ${email} registrado`);
+  response.redirect('/users/login')
+}
+
+module.exports = {renderRegistroAdoptante, registroAdoptante}
