fb01bb657768c3d024156851adcca26406fd5742
Jesus <jesusvelez@ucm.es>
2022-04-25 21:01:07 +0200

    Correcion de tests

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
-const passport = require('passport')
+// const passport = require('passport')
 const path = require('path')
 const cors = require('cors')
 const middleware = require('./utils/middleware')
@@ -12,25 +12,29 @@ const flash = require('connect-flash')
 const cookieParser = require('cookie-parser')
 const animalsRouter = require('./routes/animals.routes')
 const usersRouter = require('./routes/users.routes')
+const dotenv = require('dotenv')
+// var exphbs = require('express-handlebars');
 const Handlebars = require('handlebars')
 const expressHandlebars = require('express-handlebars');
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
-const dotenv = require('dotenv')
 const {cookieJwtAuth, authenticateToken} = require('./utils/cookieJwtAuth')
+const { Console } = require('console')
 dotenv.config({ path: './.env' });
 
 //Inicializaiones
 const app = express()
 require('./mongo')
-// require('./utils/passport')
+require('./utils/passport')
 app.use(cors())
 
 // static files
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/images', express.static(__dirname + 'public/images'))
+app.use('/uploads', express.static(__dirname + 'public/uploads'))
 app.use('/js', express.static(__dirname + 'public/js'))
 
+app.use(methodOverride('_method'))
 
 //view engine
 app.set('views', path.join(__dirname, 'views'))
@@ -49,14 +53,16 @@ app.engine(
 );
 app.set("view engine", ".hbs");
 
+
+
 // Middlewares
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(methodOverride('_method'))
-app.use(cookieParser('SecretStringForCookies'))
+app.use(cookieParser('SECRET'))
 app.use(session({
-  secret: "SecretStringForSession",
-	cookie: {	maxAge: 60000 },
+  secret: "SECRET",
+	cookie: {	maxAge: 600000 },
 	resave: true,
 	saveUninitialized: true,
   // store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
@@ -79,7 +85,7 @@ app.use((req, res, next) => {
 //Routes
 
 
-app.get('/', cookieJwtAuth,(request, response) => {  
+app.get('/',cookieJwtAuth, (request, response) => {  
   response.render('index')
 })
 app.use('/animales',cookieJwtAuth, animalsRouter)
@@ -89,7 +95,7 @@ app.use('/users',cookieJwtAuth, usersRouter)
 
 app.use(middleware.unknownEndpoint)
 // app.use(middleware.errorHandler)
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT
 
 const server = app.listen(PORT, () => {
 	logger.info(`Server running on port ${PORT}`)
